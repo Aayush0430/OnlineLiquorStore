f718902985ee7bd0c94f0ddbb8573106bdc777f7
cyruu <cyruz.mhr09@gmail.com>
2024-04-30 12:52:41 +0545

    search feature

--- jsfiles/headerSearch.js
+++ jsfiles/headerSearch.js
@@ -2,10 +2,25 @@ const searchContent = document.querySelector(".searchcontent");
 const searchIcon = document.querySelector(".searchicon");
 const closeIcon = document.querySelector(".closeicon");
 const searchInput = document.getElementById("searchinput");
+
+const searchResults = document.querySelector(".searchresults");
 searchIcon.addEventListener("click", () => {
   searchContent.classList.add("show");
 });
 closeIcon.addEventListener("click", () => {
   searchContent.classList.remove("show");
   searchInput.value = "";
+});
+searchInput.addEventListener("focus", () => {
+  searchResults.classList.add("showresults");
+});
+// searchInput.addEventListener("click", () => {
+//   if (searchInput.value.trim() === "") {
+//     console.log("empty");
+//   } else {
+//     console.log(searchInput.value);
+//   }
+// });
+searchInput.addEventListener("blur", () => {
+  searchResults.classList.remove("showresults");
 });
