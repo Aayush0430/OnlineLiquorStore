45100aec72559313d289ea035301981245bac11d
Aayush Prajapati <aayush.prajapati0430@gmail.com>
2024-05-06 19:20:39 +0545

    build

--- index.php
+++ index.php
@@ -12,8 +12,12 @@ include("./phpfiles/dbconnect.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css"
         integrity="sha512-OQDNdI5rpnZ0BRhhJc+btbbtnxaj+LdQFeh0V9/igiEPDiWE2fG+ZsXl0JEH+bjXKPJ3zcXqNyP4/F/NegVdZg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css"
+        integrity="sha512-OQDNdI5rpnZ0BRhhJc+btbbtnxaj+LdQFeh0V9/igiEPDiWE2fG+ZsXl0JEH+bjXKPJ3zcXqNyP4/F/NegVdZg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/index.css" />
     <link rel="stylesheet" href="./css/header-footer.css" />
+    <link rel="stylesheet" href="./css/card.css">
     <!-- jsquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
@@ -25,6 +29,56 @@ include("./phpfiles/dbconnect.php");
     <?php include("header.php")?>
     <div class="banner"></div>
     <?php include("trending.php")?>
+    <div class="trending">
+        <h1>Trending Products</h1>
+        <div class="trending_products">
+            <a href="">
+                <div class="cardbox">
+                    <img src="https://cheers.com.np/uploads/products/8129311194908114139335443628438136997322.png"
+                        alt="Products" class="product_image">
+                    <div class="card_details">
+                        <p class="name">Arna Beer</p>
+                        <p class="price">240</p>
+
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="cardbox">
+                    <img src="https://cheers.com.np/uploads/products/8129311194908114139335443628438136997322.png"
+                        alt="Products" class="product_image">
+                    <div class="card_details">
+                        <p class="name">Arna Beer</p>
+                        <p class="price">240</p>
+
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="cardbox">
+                    <img src="https://cheers.com.np/uploads/products/8129311194908114139335443628438136997322.png"
+                        alt="Products" class="product_image">
+                    <div class="card_details">
+                        <p class="name">Arna Beer</p>
+                        <p class="price">240</p>
+
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="cardbox">
+                    <img src="https://cheers.com.np/uploads/products/8129311194908114139335443628438136997322.png"
+                        alt="Products" class="product_image">
+                    <div class="card_details">
+                        <p class="name">Arna Beer</p>
+                        <p class="price">240</p>
+
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <?php include("footer.php"); ?>
 </body>
 <script>
 $(document).ready(function() {
