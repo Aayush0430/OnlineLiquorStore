9b9e3e7664575b024182c102864c5fc7fcd42a15
cyruu <cyruz.mhr09@gmail.com>
2024-05-07 12:54:01 +0545

    next

--- phpfiles/products.php
+++ phpfiles/products.php
@@ -3,22 +3,37 @@
     include("./dbconnect.php");
     $categoryId = $_GET['cid'];
 ?>
+
+
 <!-- jquery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
     integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <link rel="stylesheet" href="../css/products.css">
+<link rel="stylesheet" href="../css/header.css">
+
 <div id="productContainer">
     <div id="filterContainer">
         <div class="filterPrice">
-            min <input type="number" id="minPrice">
-            max <input type="number" id="maxPrice">
-            <br>
+            <p class="filtertitle">Price Range</p>
+            <div class="filterlabel">
+                <div class="label">
+                    Min
+                </div>
+                <input type="number" id="minPrice">
+            </div>
+            <div class="filterlabel">
+                <div class="label">
+                    Max
+                </div>
+                <input type="number" id="maxPrice">
+            </div>
+
             <div class="error"></div>
-            <button id="findButton">find</button>
+            <button id="findButton">Find</button>
         </div>
         <div class="hightolow">
-            <p>Price</p>
+            <p class="filtertitle">OrderBy</p>
             <button class="highlowbutton" id="hightolow">High to Low</button>
             <br>
             <button class="highlowbutton" id="lowtohigh">Low to High</button>
